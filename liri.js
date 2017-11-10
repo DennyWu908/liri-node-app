fea831b1f2015fb5dd284edbd53b8b6db6689519
Denny Wu <denny.wu87@gmail.com>
2017-11-09 23:12:28 -0800

    Corrected code for do-what-it-says command

--- liri.js
+++ liri.js
@@ -24,154 +24,106 @@ var commandOne = []
 var commandTwo = []
 
 if (input === "movie-this") {
-
-    var omdbQuery = process.argv[3];
-
-    if (omdbQuery === undefined) {
-
-        request("http://www.omdbapi.com/?t=" + "Mr. Nobody" + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
-
-            body = JSON.parse(body)
-
-            if (!error && response.statusCode === 200) {
-
-                console.log(body.Title)
-                console.log(body.Year)
-                console.log(body.Rated)
-                console.log(body.Ratings[1].Value)
-                console.log(body.Country)
-                console.log(body.Language)
-                console.log(body.Plot)
-                console.log(body.Actors)
-
-            };
-
-        });
-
-    } else {
-
-        request("http://www.omdbapi.com/?t=" + omdbQuery + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
-
-            body = JSON.parse(body)
-
-            if (!error && response.statusCode === 200) {
-
-                console.log(body.Title)
-                console.log(body.Year)
-                console.log(body.Rated)
-                console.log(body.Ratings[1].Value)
-                console.log(body.Country)
-                console.log(body.Language)
-                console.log(body.Plot)
-                console.log(body.Actors)
-
-            };
-
-        });
-
-    };
-} else if (input === "do-what-it-says") {
-
-    fs.readFile("random.txt", "utf8", function(error, response) {
-
-        if (error) {
-
-            console.log(error)
-
-        } else {
-            console.log(response)
-
-            var restart
-
-            if (response.indexOf(",") === -1) {
-
-                for (var i = 0; i < response.length; i++) {
-
-                    if (response[i] !== ",") {
-                        commandOne.push(response[i])
-                    } else {
-                        // console.log(commandOne)
-                        restart = i
-                        break
-                    }
-                };
-
-                commandOne = commandOne.join("")
-
-            } else {
-
-                for (var i = 0; i < response.length; i++) {
-
-                    if (response[i] !== ",") {
-                        commandOne.push(response[i])
-                    } else {
-                        // console.log(commandOne)
-                        restart = i
-                        break
-                    }
-                };
-
-                commandOne = commandOne.join("")
-
-                for (var i = restart + 2; i < response.length; i++) {
-                    commandTwo.push(response[i])
-                };
-
-                commandTwo = commandTwo.join("")
-
-            }
-
-            if (commandOne === "movie-this") {
-
-                var omdbQuery = commandTwo;
-                // console.log(omdbQuery.length)
-
-                if (omdbQuery === undefined) {
-
-                    request("http://www.omdbapi.com/?t=" + "Mr. Nobody" + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
-
-                        body = JSON.parse(body)
-
-                        if (!error && response.statusCode === 200) {
-
-                            console.log(body.Title)
-                            console.log(body.Year)
-                            console.log(body.Rated)
-                            console.log(body.Ratings[1].Value)
-                            console.log(body.Country)
-                            console.log(body.Language)
-                            console.log(body.Plot)
-                            console.log(body.Actors)
-
-                        };
-
-                    });
-
-                } else {
-
-                    request("http://www.omdbapi.com/?t=" + omdbQuery + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
-
-                        body = JSON.parse(body)
-
-                        if (!error && response.statusCode === 200) {
-
-                            console.log(body.Title)
-                            console.log(body.Year)
-                            console.log(body.Rated)
-                            console.log(body.Ratings[1].Value)
-                            console.log(body.Country)
-                            console.log(body.Language)
-                            console.log(body.Plot)
-                            console.log(body.Actors)
-
-                        };
-
-                    });
-
-                };
-            }
-
+  var omdbQuery = process.argv[3];
+  
+  if (omdbQuery === undefined) {    
+    request("http://www.omdbapi.com/?t=" + "Mr. Nobody" + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+      body = JSON.parse(body)      
+      if (!error && response.statusCode === 200) {
+        console.log(body.Title)
+        console.log(body.Year)
+        console.log(body.Rated)
+        console.log(body.Ratings[1].Value)
+        console.log(body.Country)
+        console.log(body.Language)
+        console.log(body.Plot)
+        console.log(body.Actors)
+      };
+    });
+  } 
+  else {
+    request("http://www.omdbapi.com/?t=" + omdbQuery + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+      body = JSON.parse(body)
+      if (!error && response.statusCode === 200) {
+        console.log(body.Title)
+        console.log(body.Year)
+        console.log(body.Rated)
+        console.log(body.Ratings[1].Value)
+        console.log(body.Country)
+        console.log(body.Language)
+        console.log(body.Plot)
+        console.log(body.Actors)
+      };
+    });
+  };
+} 
+else if (input === "do-what-it-says") {
+  fs.readFile("random.txt", "utf8", function(error, response) {
+    if (error) {
+      console.log(error)
+    } 
+    else {
+      // console.log(response)
+      var restart
+      if (response.indexOf(",") === -1) {
+        for (var i = 0; i < response.length; i++) {          
+            commandOne.push(response[i])
         }
+        commandOne = commandOne.join("")
+      }       
+      else {
+        for (var i = 0; i < response.length; i++) {
+            if (response[i] !== ",") {
+                commandOne.push(response[i])
+            } 
+            else {
+            // console.log(commandOne)
+            restart = i
+            break
+          }
+        }
+        commandOne = commandOne.join("")
+        for (var i = restart + 2; i < response.length; i++) {
+          commandTwo.push(response[i])
+        }
+        commandTwo = commandTwo.join("")
+        console.log(commandOne);
+        console.log(commandTwo);
+      }
 
-    })
+      if (commandOne === "movie-this") {
+        var omdbQuery = commandTwo;
+        if (omdbQuery.length === 0) {
+          request("http://www.omdbapi.com/?t=" + "Mr. Nobody" + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+            body = JSON.parse(body)
+            if (!error && response.statusCode === 200) {
+              console.log(body.Title)
+              console.log(body.Year)
+              console.log(body.Rated)
+              console.log(body.Ratings[1].Value)
+              console.log(body.Country)
+              console.log(body.Language)
+              console.log(body.Plot)
+              console.log(body.Actors)
+            }
+          });
+        } 
+        else {
+          request("http://www.omdbapi.com/?t=" + omdbQuery + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+            body = JSON.parse(body)
+            if (!error && response.statusCode === 200) {
+              console.log(body.Title)
+              console.log(body.Year)
+              console.log(body.Rated)
+              console.log(body.Ratings[1].Value)
+              console.log(body.Country)
+              console.log(body.Language)
+              console.log(body.Plot)
+              console.log(body.Actors)
+            };
+          });
+        }
+      }
+    }
+  });
 }
