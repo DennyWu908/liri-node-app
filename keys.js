1ead6cffad4734655018087ad1b08e329c303f74
Denny Wu <denny.wu87@gmail.com>
2018-01-06 00:30:41 -0800

    Added twitter and spotify api keys and secrets

--- keys.js
+++ keys.js
@@ -1,16 +1,16 @@
 var twitterKeys = {
 
-	consumer_key: '',
-  	consumer_secret: '',
-  	access_token_key: '',
-  	access_token_secret: ''
+	consumer_key: '9mwbAsmCmMV9UtZUoht4Uur8Q',
+  	consumer_secret: '9pPmQwRCN5omEXLjyYvzeNDUz25f8iCDasg2FXUlPGI3zQBCNU',
+  	access_token_key: '928887134777368576-X8m4oN1jyqjfcYoNfYseTdDYZimUbWk',
+  	access_token_secret: 'mP798G6nh5HT2vQNqK1Fglox0MtNJ9FxUi3JTmF6ilpn0'
 
 }
 
 var spotifyKeys = {
 
-	id: <your spotify client id>,
-  	secret: <your spotify client secret>
+	id: "25cff214624d499580e0fc4df05a2f07",
+  	secret: "befcc61f6d1a4ebf9da0231161ded9a4"
 
 }
 
